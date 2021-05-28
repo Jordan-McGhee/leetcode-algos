b1088197a7683b9dd254b2a1d4ec1bd5b94ad3e0
Jordan McGhee <jordanmcghee95@gmail.com>
2021-05-28 18:34:29 -0400

    Added may 28th practice pt 2

--- May21/LC_528.py
+++ May21/LC_528.py
@@ -36,7 +36,7 @@ class ParkingSystem(object):
 
     def addCar(self, carType):
         # for each car type, check if there's space in the parking system. If so, subtract one space and return True. If not, return False
-        
+
         if carType == 1:
             if self.big >= 1:
                 self.big -= 1
@@ -54,3 +54,39 @@ class ParkingSystem(object):
                 self.small -= 1
                 return True
             return False
+
+
+"""
+
+#1365
+How Many Numbers Are Smaller Than the Current Number
+
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+
+Example:
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation: 
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1). 
+For nums[3]=2 there exist one smaller number than it (1). 
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+"""
+
+def smallerNumbersThanCurrent(nums):
+    answer = []
+
+    for i in range(len(nums)):
+        smaller = 0
+        for num in nums:
+            if num < nums[i]:
+                smaller += 1
+        answer.append(smaller)
+
+    return answer
+
+# print(smallerNumbersThanCurrent([8,1,2,2,3]))
