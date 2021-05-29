caedde05d51139d159ecc6cc6a94fb52dd67855f
Jordan McGhee <jordanmcghee95@gmail.com>
2021-05-28 21:07:15 -0400

    Added may 28th practice pt 7

--- May21/LC_528.py
+++ May21/LC_528.py
@@ -244,7 +244,48 @@ def decompressList(nums):
         else:
             odd = num
             answer += [odd]*even
-    
+
     return answer
 
-print(decompressList([1,2,3,4]))
+# print(decompressList([1,2,3,4]))
+
+
+"""
+
+#1389
+Create Target Array in the Given Order
+
+Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+Initially target array is empty.
+From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+
+It is guaranteed that the insertion operations will be valid.
+
+Example:
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+Explanation:
+nums       index     target
+0            0        [0]
+1            1        [0,1]
+2            2        [0,1,2]
+3            2        [0,1,3,2]
+4            1        [0,4,1,3,2]
+
+"""
+
+def createTargetArray(nums, index):
+    # create empty list to store our values
+    target = []
+
+    # iterate using i to track indices
+    for i in range(len(nums)):
+        # use in-built function to insert nums[i] at index[i]
+        target.insert(index[i], nums[i])
+
+    return target
+
+# print(createTargetArray([0,1,2,3,4], [0,1,2,2,1]))
