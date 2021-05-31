97dfd1b5671ff8e7e0a91e865c06bf1cbd44f625
Jordan McGhee <jordanmcghee95@gmail.com>
2021-05-30 20:42:28 -0400

    Added may 30th practice part 4

--- May21/LC_530.py
+++ May21/LC_530.py
@@ -127,3 +127,30 @@ def maxDepth(s):
     return maximum
 
 # print(maxDepth("(1+(2*3)+((8)/4))+1"))
+
+"""
+
+#1662
+Check if Two String Arrays are Equivalent
+
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+A string is represented by an array if the array elements concatenated in order forms the string.
+
+Example:
+Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+Output: true
+Explanation:
+word1 represents string "ab" + "c" -> "abc"
+word2 represents string "a" + "bc" -> "abc"
+The strings are the same, so return true.
+
+"""
+
+def arrayStringsAreEqual(word1, word2):
+    # create two variables to compare at the end. Join the strings in our two arrays
+    one = "".join(word1)
+    two = "".join(word2)
+
+    # will return True if one = two, False if not
+    return one == two
