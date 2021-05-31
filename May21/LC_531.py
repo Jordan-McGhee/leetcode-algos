7bac6ff025c3efb5fecd2e02ffffca516c9ff700
Jordan McGhee <jordanmcghee95@gmail.com>
2021-05-31 11:56:50 -0400

    Added may 31st practice part 4

--- May21/LC_531.py
+++ May21/LC_531.py
@@ -104,3 +104,32 @@ Output: hello
 def toLowerCase(s):
     # Easy problem. Use the lower function
     return s.lower()
+
+"""
+
+#1816
+Truncate Sentence
+
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+Example:
+Input: s = "Hello how are you Contestant", k = 4
+Output: "Hello how are you"
+Explanation:
+The words in s are ["Hello", "how" "are", "you", "Contestant"].
+The first 4 words are ["Hello", "how", "are", "you"].
+Hence, you should return "Hello how are you".
+
+"""
+
+def truncateSentence(s,k):
+    # split up the given string into a list
+    lst = s.split()
+
+    # use join to create our truncated sentence, slicing off the words that come after k
+    return " ".join(lst[:k])
+
+print(truncateSentence("Hello how are you Contestant", 4))
